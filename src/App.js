2c89e5315be8679f4d62e69fe14a161943764344
Anna <anna.spalek@icloud.com>
2022-06-05 17:13:58 +0200

    added list of ingredients

--- src/App.js
+++ src/App.js
@@ -15,15 +15,33 @@ import {useState} from 'react';
   useEffect(function() {
     const fetchRecipe = async function () {
       const response = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
-      console.log(response.data.meals[0]);
+      // console.log(response.data.meals[0]);
       setRecipe(response.data.meals[0]);
     };
      fetchRecipe();
 
 
+
     
   },[]);
   
+  let arrIngridients = [];
+  for (let i=1; i<20; i++) {
+    const currIng = recipe[`strIngredient${i}`];
+    console.log(currIng);
+    if (currIng !== "" && currIng !== null) {
+      arrIngridients.push([currIng, recipe[`strMeasure${i}`]]);
+    }
+  } 
+  console.log(arrIngridients);
+  
+  const ingridients = arrIngridients.map(function(item) {
+    return (
+      <li>
+        {item[0]} : {item[1]}
+      </li>
+    )
+  } );
   
 
   return (
@@ -31,11 +49,18 @@ import {useState} from 'react';
       <h2><center>MILC(Mom! I'm a little Cook!)</center></h2>
       <div className='block'>
         <div>
-        <img src={recipe.strMealThumb} alt="meal" class="centerImg" width="500" height="600"></img>
+        <img src={recipe.strMealThumb} alt="meal" className="centerImg" width="500" height="600"></img>
         </div>
         <h2><span>{recipe.strMeal}</span></h2>
         <span>{recipe.strInstructions}</span>
+        <h3>ingridients:</h3>
+        <ul>
+          {ingridients}
+        </ul>
+        
+       
         </div>
+        
     </div>
     
     // <div className="App">
